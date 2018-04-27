3e5e8086e7406dec40b089dc1ec0d0d7f4dbd3d5
JonatanPizarro <jonatan.pizarro1998@gmail.com>
2018-04-27 20:28:59 +0200

    a

---
+++ ajedrezCordova/ajedrez/platforms/browser/www/js/script.js
@@ -0,0 +1,133 @@
+$('#userName').val('hola');
+$('#userPassword').val('asdasd');
+
+
+
+//login
+$('#submit').click(function(){
+    var user = $('#userName').val();
+    var pass = $('#userPassword').val();
+    
+
+        $.ajax({
+            type: 'GET',
+            url: 'https://ajedrezap.herokuapp.com/api/login/'+user+'/'+pass,
+           
+
+            success: function (msg) {
+                
+                
+                msg = JSON.parse(msg);
+                alert(msg.token);
+                alert(msg.id);
+                
+                if( msg.estado == 'ok'){
+                    localStorage.setItem("token", msg.token);
+                    localStorage.setItem("id" , msg.id);
+                    window.location.replace("espera.html");
+                }
+            },
+            error: function (request, status, error) {
+               
+                alert(JSON.stringify(request));
+            }
+        });
+    });
+
+
+
+
+$('#lista').click(function(){
+        
+        $.ajax({
+            type: 'GET',
+            url: 'https://ajedrezap.herokuapp.com/api/en_espera/'+localStorage.getItem("token"),
+
+
+            success: function (msg) {       
+                alert('https://ajedrezap.herokuapp.com/api/en_espera/'+localStorage.getItem("token"));
+                msg = JSON.parse(msg);
+               // alert(msg);
+                var tablero= $("#tabla");
+                $("#tabla tr").remove();
+
+                for (var i =0; i<msg.nombre.length;i++){
+                    var td =$("<td>"+msg.nombre[i].name+"</td>");
+                    var td1 =$("<td></td>");
+                    var boton =$ ("<button id='"+msg.id[i].id+"' >Jugar</button>").click(function(e) { crearPartida(e.target.id);});
+                    var tr =$("<tr></tr>");
+                    tablero.append(tr);
+                    tr.append(td);
+                    td1.append(boton);
+                    tr.append(td1);
+                }
+
+
+                
+            },
+            error: function (request, status, error) {
+
+                alert(JSON.stringify(request));
+            }
+        });
+    });
+
+
+
+$('#partidas').click(function(){
+
+    setInterval(ajaxCall, 2000); //2 sec"
+
+                function ajaxCall() {
+                    $.ajax({
+                            type: 'GET',
+                            url: 'https://ajedrezap.herokuapp.com/api/comprovar_partida/'+localStorage.getItem("id")+'/'+localStorage.getItem("token"),
+
+
+                            success: function (msg) {  
+                                    
+                               msg = JSON.parse(msg);
+                               alert(msg.idPartida);
+                               if (msg.estado == 'Partida encontrada'){
+                                localStorage.setItem("idPartida", msg.idPartida);
+                                window.location.replace("tablero.html");
+
+
+                               };              
+                                         
+                                
+                            },
+                            error: function (request, status, error) {
+                                
+                            }
+                        });
+                };
+});
+
+
+
+function crearPartida(e){  
+    
+        alert(e);
+        $.ajax({
+            type: 'GET',
+            url: 'https://ajedrezap.herokuapp.com/api/crear_partida/'+localStorage.getItem("id")+'/'+e+'/'+localStorage.getItem("token"),
+
+
+            success: function (msg) {  
+                alert('https://ajedrezap.herokuapp.com/api/crear_partida/'+localStorage.getItem("id")+'/'+e+'/'+localStorage.getItem("token")) ;    
+               
+                window.location.replace("tablero.html");
+                         
+                
+            },
+            error: function (request, status, error) {
+                alert('https://ajedrezap.herokuapp.com/api/crear_partida/'+localStorage.getItem("id")+'/'+e+'/'+localStorage.getItem("token")) ;    
+                alert(JSON.stringify(request));
+            }
+        });
+    };
+
+
+
+
